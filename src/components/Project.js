e323c5c8923030020ecc1231d04488c4eecace0a
Yash Pathak <90676914+Yash245Pathak@users.noreply.github.com>
2023-04-13 09:35:39 +0530

    update

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,13 +5,15 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import img1 from '../assets/img-1.png'
 import img2 from '../assets/img-2.png'
 import img3 from '../assets/img-3.png'
+import blog from '../assets/blog.png'
+import portfolio from '../assets/portfolio.png'
 
 
 const Project = () => {
   return <section className='section mb-32' id='work'>
     <div className="container mx-auto text-center lg:text-left">
       <div className='flex flex-col gap-y-10 gap-x-10 lg:flex-row justify-center items-center'>
-        <motion.div variants={fadeIn('right', 0.5)} initial='hidden' whileInView={'show'} className='flex-1 gap-y-12 cursor-pointer'>
+        <motion.div variants={fadeIn('up', 0.5)} initial='hidden' whileInView={'show'} className='flex-1 gap-y-12 cursor-pointer'>
           <div>
             <h1 className='text-accent font-primary text-[65px]  leading-tight font-bold mx-auto max-w-max lg:mx-0 mb-4'>Portfolio.</h1>
             <p className='capitalize leading-tight text-[28px] font-secondary font-semibold'>view All My Work:</p>
@@ -32,9 +34,15 @@ const Project = () => {
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient font-bold'>Beautiful Landing Page</span></div>
               <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
             </div>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl mt-[70px]'>
+              <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
+              <img className='group-hover:scale-150 transition-all duration-500' src={portfolio} alt="" />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient font-bold'>Portfolio Website</span></div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
+            </div>
           </div>
         </motion.div>
-        <motion.div variants={fadeIn('left', 0.5)} initial='hidden' whileInView={'show'} className='flex-1 gap-y-12 cursor-pointer'>
+        <motion.div variants={fadeIn('up', 0.5)} initial='hidden' whileInView={'show'} className='flex-1 gap-y-12 cursor-pointer'>
           <div className='flex flex-col gap-y-6'>
             <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
@@ -45,7 +53,13 @@ const Project = () => {
             <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
               <img className='group-hover:scale-150 transition-all duration-500' src={img3} alt="" />
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient font-bold'>NEWSCATCH.com</span></div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient font-bold'>NFT Website Landing Page</span></div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
+            </div>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
+              <img className='group-hover:scale-150 transition-all duration-500' src={blog} alt="" />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient font-bold'>Full Stack Blog Website</span></div>
               <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
             </div>
           </div>
